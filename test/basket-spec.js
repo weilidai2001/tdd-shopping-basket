aa13d22525e13f4ebb5c442d35e2a8b953e31ebc
weilidai <weilidai2001@hotmail.com>
2016-09-27 16:09:24 +0100

    Added unit test place holders that shall cover all business logic paths expected

---
+++ test/basket-spec.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const should = require("chai").should();
+
+describe("Basket senarios", function () {
+  it("should calculate the total cost of items that are not on sale");
+
+  it("should calculate the total cost of items that are three for the price of two");
+
+  it("should calculate the total cost of items that three for the price of two applies more than once");
+
+  it("should calculate the total cost of items that are a mixture of discounted and non-discounted");
+
+});
