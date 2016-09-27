ddaec6733c53f53c12daa7645ecbad38346e7501
weilidai <weilidai2001@hotmail.com>
2016-09-27 17:13:58 +0100

    Fixed bug revealed by third test

--- src/discount-rules/three-papays-for-price-of-two.js
+++ src/discount-rules/three-papays-for-price-of-two.js
@@ -2,6 +2,6 @@ const itemPriceList = require("../items-price-list");
 
 module.exports = {
   calculateDiscount(basket){
-    return itemPriceList.getPriceByItemName("papaya") * Math.floor(basket.countItemsByType("papaya") / 2);
+    return itemPriceList.getPriceByItemName("papaya") * Math.floor(basket.countItemsByType("papaya") / 3);
   }
 };
